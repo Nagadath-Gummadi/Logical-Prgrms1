dd9cac73ef5668180937f34b5a75b288604f1bb0
Nagadath-Gummadi <51068943+Nagadath-Gummadi@users.noreply.github.com>
2019-08-20 12:19:34 +0530

    Add files via upload

---
+++ max_in_seq_command_line_args.c
@@ -0,0 +1,61 @@
+# include<stdlib.h>
+int main(int argc,char* argv[])
+{
+    int *a,n,i,count=0,max=0,occ=0,co;
+    printf("Program Name Is: %s",argv[0]);
+	if(argc==1)
+		printf("\nNo Extra Command Line Argument Passed Other Than Program Name");
+    a=(int*)malloc(argc*sizeof(int));
+	if(argc>=2)
+	{
+		printf("\n----Following Are The Command Line Arguments Passed----");
+		for(co=0;co<argc-1;co++)
+		{
+		    printf("\n argv[%d]: %s",co+1,argv[co+1]);
+		    a[co]=abs(argv[co+1][0])-'0';
+		}
+	}
+	n=argc-1;
+	printf("\n");
+	for(i=0;i<n;i++)
+        printf("  %d  ",a[i]);
+    printf("\n");
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<=a[i+1])
+        {
+            ++count;
+            printf("%d  ",a[i]);
+        }
+        else
+        {
+
+            if(a[i+1]<0)
+            {
+                printf("%d  ",a[i]);
+                printf("  ->%d\n",++count);
+                occ=1;
+                 break;
+            }
+            else
+            {
+                printf("%d  ",a[i]);
+                count++;
+            }
+
+                printf("  ->%d\n",count);
+            if(max<count)
+                max=count;
+            count=0;
+        }
+      }
+
+      if(max<count)
+            max=count;
+        if(occ)
+            printf("\nMaximum %d",max);
+      return 0;
+
+
+
+}
---
+++ sum_of_sq_of_mat_command_line_arg.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+
+int main(int argc,char* argv[])
+
+{
+
+    int r,c,i,j,rsum=0,cpro=0,n;
+
+    int co;
+	printf("Program Name Is: %s",argv[0]);
+	if(argc==1)
+		printf("\nNo Extra Command Line Argument Passed Other Than Program Name");
+	if(argc==3)
+	{
+		printf("\n----Following Are The Command Line Arguments Passed----");
+		for(co=0;co<argc;co++)
+			printf("\n argv[%d]: %s",co,argv[co]);
+	}
+	else
+    {
+        printf("Given Command Line Arguments are not suitable for this program/nPlease enter arguments as rows and cols");
+    }
+
+    r=argv[1][0];
+    r=r-'0';
+    c=argv[2][0];
+    c=c-'0';
+    printf("\nRows:%d\nCols:%d\n",r,c);
+    printf("Enter the elements:");
+    for(i=1;i<=r;i++)
+
+    {
+
+        for(j=1;j<=c;j++)
+
+        {
+
+            scanf("%d",&n);
+
+            if(j!=c)
+
+            {
+
+                rsum+=n;
+
+                printf("%d+",n);
+
+            }
+
+            else
+
+            {
+
+                printf("%d=",n);
+
+                rsum+=n;
+
+                printf("%d\n",rsum);
+
+                printf("%d*%d+%d=",rsum,rsum,cpro);
+
+                cpro+=rsum*rsum;
+
+                printf("%d\n",cpro);
+
+                rsum=0;
+
+            }
+
+        }
+
+    }
+
+    printf("\nSum of Squares:%d",cpro);
+
+    return 0;
+
+}
